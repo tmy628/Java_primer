0b695e7d065e3041bf32c071e4db526b50642e3a
tmy628 <tmy6h1r@gmail.com>
2021-08-18 11:59:18 +0900

    [fix] Chapter1 1-4/1-5

--- Chapter1/Main4.java
+++ Chapter1/Main4.java
@@ -1,12 +1,14 @@
+// 書き換え防止
 public class Main4 {
     public static void main(String[] args) {
-        double pi = 3.14;
+        // final 型 定数名 = 初期値;
+        final double PI = 3.14;
         int pie = 5;
         System.out.println("半径" + pie + "cmのパイの面積は、");
-        System.out.println(pie * pie * pi);
+        System.out.println(pie * pie * PI);
         System.out.println("パイの半径を倍にします");
-        pi = 10;
+        PI = 10;
         System.out.println("半径" + pie + "cmのパイの面積は、");
-        System.out.println(pie * pie * pi);
+        System.out.println(pie * pie * PI);
     }
 }
