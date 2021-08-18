3c1a9d2666574c7c8bd5b535100df90afd1dadbe
tmy628 <tmy6h1r@gmail.com>
2021-08-18 11:35:24 +0900

    [add] Chapter1 1-3

---
+++ Chapter1/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main (String[] args) {
+        int age;
+        age = 20;
+        System.out.println("私の年齢は" + age);
+        age = 31;
+        System.out.println("・・・いや、本当の年齢は" + age);
+    }
+}
