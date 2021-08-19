82900fb2bd58d1d8c94c9928fab83052899b2716
tmy628 <tmy6h1r@gmail.com>
2021-08-19 21:32:18 +0900

    [add] Chapter1 Exercises

---
+++ Chapter1/Main5.java
@@ -0,0 +1,15 @@
+// 1-2
+public class Main5 {
+    public static void main(String[] args) {
+        int a = 3; int b = 5;
+        int c = a * b;
+        System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+    }
+}
+
+// 1-3
+// boolean result = true;
+// char favoriteCharacter = '駆';
+// final double PI = 3.14;
+// long number = 314159265853979L;
+// String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
