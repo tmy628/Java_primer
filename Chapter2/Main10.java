a02bb676c08b1b9fa5a5a2c216d212790fb79cbb
tmy628 <tmy6h1r@gmail.com>
2021-08-21 09:31:19 +0900

    [add] Chapter2 2-10

---
+++ Chapter2/Main10.java
@@ -0,0 +1,8 @@
+public class Main10 {
+    public static void main(String[] args) {
+        String name = "すがわら";
+        String message;
+        message = name + "さん、こんにちは";
+        System.out.println(message);
+    }
+}
